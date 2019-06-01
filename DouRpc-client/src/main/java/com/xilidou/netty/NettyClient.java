c43fe16b8b074145c13f266043f89a3e74f2d55b
weijuntao <weijuntao@aobi.com>
2019-06-01 17:40:51 +0800

    Revert "[style]"
    This reverts commit a3e73a87674dc395b7f28e9426e89e9369cde76c.

--- DouRpc-client/src/main/java/com/xilidou/netty/NettyClient.java
+++ DouRpc-client/src/main/java/com/xilidou/netty/NettyClient.java
@@ -1,5 +1,7 @@
 package com.xilidou.netty;
 
+import java.net.InetSocketAddress;
+
 import com.xilidou.client.Client;
 import com.xilidou.coder.RpcDecoder;
 import com.xilidou.coder.RpcEncoder;
@@ -7,6 +9,7 @@ import com.xilidou.entity.RpcRequest;
 import com.xilidou.entity.RpcResponse;
 import com.xilidou.netty.handler.ClientHandler;
 import com.xilidou.serialization.JsonSerialization;
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
@@ -18,68 +21,64 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 
-import java.net.InetSocketAddress;
-
 /**
  * @author Zhengxin
  */
 public class NettyClient implements Client {
 
-    private EventLoopGroup eventLoopGroup;
-    private Channel channel;
-    private ClientHandler clientHandler;
+	private EventLoopGroup eventLoopGroup;
+	private Channel channel;
+	private ClientHandler clientHandler;
 
-    private String host;
-    private int port;
+	private String host;
+	private int port;
 
-    public NettyClient(String host, int port) {
-        this.port = port;
-        this.host = host;
-    }
+	public NettyClient(String host, int port) {
+		this.port = port;
+		this.host = host;
+	}
 
-    @Override
-    public RpcResponse send(RpcRequest request) {
-        try {
-            channel.writeAndFlush(request).await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return clientHandler.getRpcResponse(request.getRequestId());
-    }
+	@Override
+	public RpcResponse send(final RpcRequest request) {
+		try {
+			channel.writeAndFlush(request).await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return clientHandler.getRpcResponse(request.getRequestId());
+	}
 
-    @Override
-    public void connect(InetSocketAddress inetSocketAddress) {
-        clientHandler = new ClientHandler();
-        eventLoopGroup = new NioEventLoopGroup();
-        Bootstrap bootstrap = new Bootstrap();
-        bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class).option(ChannelOption.SO_KEEPALIVE, true)
-                .option(ChannelOption.TCP_NODELAY, true).handler(new ChannelInitializer<SocketChannel>() {
+	@Override
+	public void connect(final InetSocketAddress inetSocketAddress) {
+		clientHandler = new ClientHandler();
+		eventLoopGroup = new NioEventLoopGroup();
+		Bootstrap bootstrap = new Bootstrap();
+		bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class).option(ChannelOption.SO_KEEPALIVE, true)
+				.option(ChannelOption.TCP_NODELAY, true).handler(new ChannelInitializer<SocketChannel>() {
+					@Override
+					protected void initChannel(SocketChannel ch) throws Exception {
+						ChannelPipeline pipeline = ch.pipeline();
+						pipeline.addLast(new LengthFieldBasedFrameDecoder(65535, 0, 4));
+						pipeline.addLast(new RpcEncoder(RpcRequest.class, new JsonSerialization()));
+						pipeline.addLast(new RpcDecoder(RpcResponse.class, new JsonSerialization()));
+						pipeline.addLast(clientHandler);
+					}
+				});
+		try {
+			channel = bootstrap.connect(inetSocketAddress).sync().channel();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
-            @Override
-            protected void initChannel(SocketChannel ch) throws Exception {
-                ChannelPipeline pipeline = ch.pipeline();
-                pipeline.addLast(new LengthFieldBasedFrameDecoder(65535, 0, 4));
-                pipeline.addLast(new RpcEncoder(RpcRequest.class, new JsonSerialization()));
-                pipeline.addLast(new RpcDecoder(RpcResponse.class, new JsonSerialization()));
-                pipeline.addLast(clientHandler);
-            }
+	@Override
+	public InetSocketAddress getInetSocketAddress() {
+		return new InetSocketAddress(host, port);
+	}
 
-        });
-        try {
-            channel = bootstrap.connect(inetSocketAddress).sync().channel();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public InetSocketAddress getInetSocketAddress() {
-        return new InetSocketAddress(host, port);
-    }
-
-    @Override
-    public void close() {
-        eventLoopGroup.shutdownGracefully();
-        channel.closeFuture().syncUninterruptibly();
-    }
+	@Override
+	public void close() {
+		eventLoopGroup.shutdownGracefully();
+		channel.closeFuture().syncUninterruptibly();
+	}
 }
--- DouRpc-testclient/src/main/java/com/xilidou/ClientApplication.java
+++ DouRpc-testclient/src/main/java/com/xilidou/ClientApplication.java
@@ -1,17 +1,18 @@
 package com.xilidou;
 
-import com.xilidou.api.IHelloService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.xilidou.api.IHelloService;
+
 @SpringBootApplication()
 public class ClientApplication {
 
-    public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(ClientApplication.class);
-        IHelloService helloService = context.getBean(IHelloService.class);
-        System.out.println(helloService.sayHi("dubbo"));
-    }
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(ClientApplication.class);
+		IHelloService helloService = context.getBean(IHelloService.class);
+		System.out.println(helloService.sayHi("doudou"));
+	}
 
 }
